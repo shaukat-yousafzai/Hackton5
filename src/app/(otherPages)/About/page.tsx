fc4f8a78b1b2b9c3342211d1f18f075659856ae9
Shaukat Khan <shoukatazizpak@gmail.com>
2024-12-10 20:58:35 +0500

    update2

--- src/app/(otherPages)/About/page.tsx
+++ src/app/(otherPages)/About/page.tsx
@@ -1,10 +1,253 @@
 import React from 'react'
 
+import Container from '@/components/Container'
+import Image from 'next/image'
+import { FaCookieBite, FaWineGlassAlt } from 'react-icons/fa'
+import { LiaHamburgerSolid } from 'react-icons/lia'
+
+import KingPara from '@/components/KingPara'
+
+import { PiCoffee, PiPerson, PiStudent } from 'react-icons/pi'
+
 const Aboutpage = () => {
   return (
-    <div className='text-3xl flex justify-center mt-8'>
-             Only Home Shop and Menu is complete 
+    <div className='flex flex-col mt-11'>
+          <Container className='mt-20 flex items-center justify-center ' >
+    <div className="grid grid-rows-2 gap-4 items-center justify-center">
+      {/* First Row */}
+      <div className="grid grid-cols-2 items-end gap-0">
+        {/* Big Image (Left Side) */}
+        <div className="w-[362px] h-[356px]">
+          <Image
+            src="/images/why1.svg"
+            alt="Big Image"
+            layout="responsive"
+            width={362}
+            height={356}
+            className="object-cover"
+          />
+        </div>
+
+        {/* Small Image (Right Side) */}
+        <div className="w-[281px] h-[231px]">
+          <Image
+            src="/images/why2.svg"
+            alt="Small Image 1"
+            layout="responsive"
+            width={281}
+            height={231}
+            className="object-cover"
+          />
+        </div>
+      </div>
+
+      {/* Second Row */}
+      <div className="grid grid-cols-3 items-start gap-2">
+        {/* First Column */}
+        <div className="w-[244px] h-[306px]">
+          <Image
+            src="/images/why4.svg"
+            alt="Small Image 2"
+            layout="responsive"
+            width={244}
+            height={306}
+            className="object-cover"
+          />
+        </div>
+
+        {/* Second Column */}
+        <div className="w-[221px] h-[226px]">
+          <Image
+            src="/images/why5.svg"
+            alt="Small Image 3"
+            layout="responsive"
+            width={221}
+            height={226}
+            className="object-cover"
+          />
+        </div>
+
+        {/* Third Column */}
+        <div className="grid w-[160px] grid-rows-2 gap-4 items-start justify-center">
+          <div>
+            <Image
+              src="/images/fourgrid3.svg"
+              alt="Small Image 5"
+              layout="responsive"
+              width={170}
+              height={166}
+              className="object-cover"
+            />
+          </div>
+          <div>
+            <Image
+              src="/images/why6.svg"
+              alt="Small Image 4"
+              layout="responsive"
+              width={170}
+              height={168}
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </div>
     </div>
+    <div className='flex flex-col items-start justify-center'>
+        <KingPara text='About ____'/>
+         <h3 className='text-4xl text-black font-semibold mt-2 mb-4'>
+         Food is an important 
+         <br />
+         part Of a balanced Diet
+         </h3>
+        <div className='w-[526px]'>
+       <p className='text-gray-900'>
+       Lorem ipsum dolor sit amet, consectetur gfgfg  pakist   adipiscing elit. Quisque bibendum non dui volutpat fringilla bibendum. Urna, elit augue urna, vitae feugiat pretium donec id elementum. Ultrices mattis sed vitae mus risus. Lacus nisi, et ac dapibus sit eu velit in consequat.
+       </p>
+        </div>
+        <div className='flex  justify-between w-[374px] my-5'>
+            <div>
+                <div className='w-[102px] h-[100px] font-extralight text-6xl flex rounded-md  items-center justify-center bg-prColor text-white'>
+                 <LiaHamburgerSolid/>
+                 
+                </div>
+                <p className='text-center text-white mt-2'>Fast Food</p>
+            </div>
+            <div>
+            <div className='w-[102px] h-[100px] font-extralight text-6xl flex rounded-md  items-center justify-center bg-prColor text-white'>
+                <FaCookieBite />
+             </div>
+             <p className='text-center text-white mt-2'>Lunch</p>
+            </div>
+            <div>
+            <div className='w-[102px] h-[100px]  flex rounded-md text-5xl items-center justify-center bg-prColor text-white'>
+              <FaWineGlassAlt/>
+            </div>
+            <p className='text-center text-white mt-2'>Drinner</p>
+            </div>
+            
+        </div>
+        <Image
+         src={"/images/experince.png"}
+         alt='30 year of experince'
+         height={92}
+         width={374}
+        />
+        
+    </div>
+
+    </Container>
+
+    <Container className='flex flex-col items-center justify-center mt-10'>
+    <h3 className='text-4xl text-black font-semibold mt-2 mb-4'>
+           why Choose
+         </h3>
+         <div className='w-[526px] text-center'>
+       <p className='text-gray-900'>
+      bibendum. Urna, elit augue urna, vitae feugiat sit 
+      <br /> mus risus. Lacus nisi, et ac dapibus 
+       </p>
+       </div>
+       <div className='  mt-5'>
+        <Image 
+        src={"/images/Heroback.svg"}
+        alt='this is image'
+        height={300}
+        width={1320}
+        className='object-cover'
+        />
+
+       </div>
+    </Container>
+    <Container className="grid grid-cols-3 gap-x-32 mt-10 items-center justify-center text-center"
+    >
+      <div className='flex flex-col items-center'>
+        
+ <PiStudent className='text-6xl font-normal mb-5'/>
+  <h3 className='text-3xl font-bold text-black'> Best chif</h3>
+  <p className='flex items-center justify-center mt-4'>Lorem ipsum dolor sit amet, 
+   
+    consectetur  <br /> adipiscing elit. 
+   
+     Quisque diam pellentesque  <br /> bibendum non dui volutpat </p>
+
+ 
+      </div>
+      <div className='flex flex-col items-center'>
+      <PiCoffee className='text-6xl font-normal' />
+      <h3 className='text-3xl font-bold text-black'> 120 Item food</h3>
+  <p className='flex items-center justify-center mt-4'>Lorem ipsum dolor sit amet, 
+   
+    consectetur  <br /> adipiscing elit. 
+   
+     Quisque diam pellentesque  <br /> bibendum non dui volutpat </p>
+      </div>
+      <div className='flex flex-col items-center'>
+      <PiPerson className='text-6xl font-normal' />
+      <h3 className='text-3xl font-bold text-black'> Clean Enviroment</h3>
+  <p className='flex items-center justify-center mt-4'>Lorem ipsum dolor sit amet, 
+   
+    consectetur  <br /> adipiscing elit. 
+   
+     Quisque diam pellentesque  <br /> bibendum non dui volutpat </p>
+      </div>
+    </Container>
+    <div className="relative w-full h-[300px] bg-prColor my-14">
+  {/* First Section (Header Content) */}
+  <div className="absolute w-full h-1/2 flex flex-col justify-center">
+    <h3 className="text-3xl flex justify-center font-bold text-white">Team Member</h3>
+    <p className="text-center text-white mt-4">
+      Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Quisque diam pellentesque
+    </p>
+  </div>
+
+  {/* Second Section (Grid in Container) */}
+  <div className="absolute bottom-0 translate-y-1/2 w-full">
+  <Container className=' flex  items-center justify-around'>
+  <div className="  flex items-center justify-center">
+    <Image
+    src={'/images/Mark.svg'}
+    alt='this is image'
+    height={398}
+    width={312}
+    className='object-cover'
+    />
+  </div>
+  <div className=" h-72 flex items-center justify-center">
+    <Image
+    src={'/images/Mark.svg'}
+    alt='this is image'
+    height={398}
+    width={312}
+    className='object-cover'
+    />
+  </div>
+  <div className=" h-72  flex items-center justify-center">
+    <Image
+    src={'/images/Mark.svg'}
+    alt='this is image'
+    height={398}
+    width={312}
+    className='object-cover'
+    />
+  </div>
+  <div className=" h-72 flex items-center justify-center">
+    <Image
+    src={'/images/Mark.svg'}
+    alt='this is image'
+    height={398}
+    width={312}
+    className='object-cover'
+    />
+  </div>
+  </Container>
+   
+  </div>
+</div>
+
+
+
+    </div>
+   
   )
 }
 
--- src/app/(otherPages)/Blog/page.tsx
+++ src/app/(otherPages)/Blog/page.tsx
@@ -1,10 +1,398 @@
+import Container from '@/components/Container'
+import Image from 'next/image'
 import React from 'react'
+
+
+
+import { PiArrowLineUpRight, PiCalendar, PiChats, PiFacebookLogoBold, PiInstagramLogo, PiMagnifyingGlass,  PiPinterestLogo,  PiStarBold, PiTwitchLogo, PiUserCirclePlus } from 'react-icons/pi'
+
+
 
 const BlogPage = () => {
   return (
-    <div className='text-3xl flex justify-center mt-8'>
-             Only Home Shop and Menu is complete 
+     <div>
+      <Container className='flex gap-7'>
+        <div>
+
+          <div className='mt-14'>
+
+
+            <div className='mb-2'>
+              <Image
+              src={"/images/Blog1.svg"}
+              alt='this image'
+              width={872}
+              height={520}
+              />
+            </div>
+            <p className='flex items-center gap-2'>
+              <PiCalendar className='text-prColor'/>
+                Feb 14, 2022/
+                <PiChats className='text-prColor'/>
+                3/ 
+                <PiUserCirclePlus className='text-prColor' />
+                Admin
+            </p>
+            <h3 className='text-2xl text-black font-bold'>
+            10 Reasons To Do A Digital Detox Challenge
+            </h3>
+              <div className='border w-[43.375rem] my-6 border-black h-[1px] opacity-20'>
+
+              </div>
+              <p className='w-[40.375rem] text-black font-inter text-base ' >
+              At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat
+              </p>
+              <div className="w-[10.75rem] h-[3.25rem] mt-6 text-prColor items-center border-prColor flex  p-[0.875rem] px-[2rem] gap-[0.625rem] rounded-tl-[0.375rem] border-[0.0625rem]">
+                Read More <PiArrowLineUpRight/>
+</div>
+
+          </div>
+          <div className='mt-14'>
+
+
+<div className='mb-2'>
+  <Image
+  src={"/images/Blog1.svg"}
+  alt='this image'
+  width={872}
+  height={520}
+  />
+</div>
+<p className='flex items-center gap-2'>
+  <PiCalendar className='text-prColor'/>
+    Feb 14, 2022/
+    <PiChats className='text-prColor'/>
+    3/ 
+    <PiUserCirclePlus className='text-prColor' />
+    Admin
+</p>
+<h3 className='text-2xl text-black font-bold'>
+10 Reasons To Do A Digital Detox Challenge
+</h3>
+  <div className='border w-[43.375rem] my-6 border-black h-[1px] opacity-20'>
+
+  </div>
+  <p className='w-[40.375rem] text-black font-inter text-base ' >
+  At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat
+  </p>
+  <div className="w-[10.75rem] h-[3.25rem] mt-6 text-prColor items-center border-prColor flex  p-[0.875rem] px-[2rem] gap-[0.625rem] rounded-tl-[0.375rem] border-[0.0625rem]">
+    Read More <PiArrowLineUpRight/>
+</div>
+
+        </div>
+       <div className='mt-14'>
+
+
+<div className='mb-2'>
+  <Image
+  src={"/images/Blog1.svg"}
+  alt='this image'
+  width={872}
+  height={520}
+  />
+</div>
+<p className='flex items-center gap-2'>
+  <PiCalendar className='text-prColor'/>
+    Feb 14, 2022/
+    <PiChats className='text-prColor'/>
+    3/ 
+    <PiUserCirclePlus className='text-prColor' />
+    Admin
+</p>
+<h3 className='text-2xl text-black font-bold'>
+10 Reasons To Do A Digital Detox Challenge
+</h3>
+  <div className='border w-[43.375rem] my-6 border-black h-[1px] opacity-20'>
+
+  </div>
+  <p className='w-[40.375rem] text-black font-inter text-base ' >
+  At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat
+  </p>
+  <div className="w-[10.75rem] h-[3.25rem] mt-6 text-prColor items-center border-prColor flex  p-[0.875rem] px-[2rem] gap-[0.625rem] rounded-tl-[0.375rem] border-[0.0625rem]">
+    Read More <PiArrowLineUpRight/>
+</div>
+
+      </div> 
+     <div className='mt-14'>
+
+
+<div className='mb-2'>
+  <Image
+  src={"/images/Blog1.svg"}
+  alt='this image'
+  width={872}
+  height={520}
+  />
+</div>
+<p className='flex items-center gap-2'>
+  <PiCalendar className='text-prColor'/>
+    Feb 14, 2022/
+    <PiChats className='text-prColor'/>
+    3/ 
+    <PiUserCirclePlus className='text-prColor' />
+    Admin
+</p>
+<h3 className='text-2xl text-black font-bold'>
+10 Reasons To Do A Digital Detox Challenge
+</h3>
+  <div className='border w-[43.375rem] my-6 border-black h-[1px] opacity-20'>
+
+  </div>
+  <p className='w-[40.375rem] text-black font-inter text-base ' >
+  At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat
+  </p>
+  <div className="w-[10.75rem] h-[3.25rem] mt-6 text-prColor items-center border-prColor flex  p-[0.875rem] px-[2rem] gap-[0.625rem] rounded-tl-[0.375rem] border-[0.0625rem]">
+    Read More <PiArrowLineUpRight/>
+</div>
+
+      </div>
+
+        </div>
+        <div>
+        <div className="w-[26.3625rem] h-[4.375rem] border flex mt-14">
+  {/* Input Box takes remaining width */}
+  <input 
+    type="search" 
+    placeholder="Search your keyword" 
+    className="flex-1 px-4" 
+  />
+
+  {/* Magnifying Glass Icon on the right side */}
+  <div className="w-[4.75rem] bg-prColor h-[4.375rem] border-[0.0625rem] flex justify-center items-center">
+    <PiMagnifyingGlass className="text-xl" />
+  </div>
+</div>
+<div className="w-[26.5rem] h-[26.4375rem] flex justify-center mt-6 items-center border">
+  
+  <div className="w-[20rem] h-[21.625rem] flex flex-col justify-center items-center text-center">
+    
+    <div className="w-[7.1875rem] h-[7.081875rem] overflow-hidden rounded-full mb-4">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Image" 
+        width={115} 
+        height={113.31} 
+        className="object-cover"
+      />
     </div>
+    
+  
+    <h3 className="font-helvetica text-[1.25rem] font-bold leading-[1.75rem] text-center  decoration-skip-ink mb-2">
+      Brayni developer
+    </h3>
+    
+    
+    <p className="font-inter text-[1rem] font-light leading-[1.5rem]">
+      Developer
+    </p>
+    <p className='flex gap-1 my-3'>
+      <PiStarBold className='text-prColor '/>
+      <PiStarBold className='text-prColor '/>
+      <PiStarBold className='text-prColor '/>
+      <PiStarBold className='text-prColor '/>
+      <PiStarBold className='text-prColor '/>
+      (1 Reiwie)
+    </p>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur
+adipisicing elit.Veritatis distinctio, odio
+eligendi suscipit reprehenderit atque
+    </p>
+    <div className="flex space-x-4 mt-6 w-24 h-10">
+         <PiFacebookLogoBold/>
+         <PiInstagramLogo/>
+         <PiTwitchLogo/>
+         <PiPinterestLogo/>
+</div>
+
+  </div>
+</div>
+<div className="w-[26.5rem] p-8  flex flex-col justify-start mt-6 items-start border">
+<h1 className='text-2xl font-bold text-gray-900'>
+  Recent Post </h1>
+  <div className='flex gap-3 mt-6'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div>
+    <p className='text-base font-extralight mb-2'>june 2020</p>
+    <p className='text-base font-medium mb-2'>this is image is <br /> thing this is</p>
+       
+    </div>
+  </div>
+  <div className='flex gap-3 mt-6'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div>
+    <p className='text-base font-extralight mb-2'>june 2020</p>
+    <p className='text-base font-medium mb-2'>this is image is <br /> thing this is</p>
+       
+    </div>
+  </div>
+  <div className='flex gap-3 mt-6'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div>
+    <p className='text-base font-extralight mb-2'>june 2020</p>
+    <p className='text-base font-medium mb-2'>this is image is <br /> thing this is</p>
+       
+    </div>
+  </div>
+  <div className='flex gap-3 mt-6'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div>
+    <p className='text-base font-extralight mb-2'>june 2020</p>
+    <p className='text-base font-medium mb-2'>this is image is <br /> thing this is</p>
+       
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div className="w-[26.5rem] p-8  flex flex-col justify-start mt-6 items-start border">
+<h1 className='text-2xl font-bold text-gray-900'>
+  Recent Post </h1>
+  <div className='flex gap-3 mt-6 items-center'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div className='flex justify-between space-x-32'>
+    <p className='text-xl font-bold '>Chicken </p>
+    <p className='text-lg font-medium '>20$</p>
+       
+    </div>
+  </div>
+  <div className='flex gap-3 mt-6 items-center'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div className='flex justify-between space-x-32'>
+    <p className='text-xl font-bold '>Chicken </p>
+    <p className='text-lg font-medium '>20$</p>
+       
+    </div>
+  </div>
+  <div className='flex gap-3 mt-6 items-center'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div className='flex justify-between space-x-32'>
+    <p className='text-xl font-bold '>Chicken </p>
+    <p className='text-lg font-medium '>20$</p>
+       
+    </div>
+  </div>
+  <div className='flex gap-3 mt-6 items-center'>
+  <div className="flex justify-center items-center ">
+      <Image
+        src={"/images/why1.svg"}
+        alt="Description of the image"
+        width={99}
+        height={91.58} 
+        className="object-cover rounded-md" 
+      />
+    </div>
+    <div className='flex justify-between space-x-32'>
+    <p className='text-xl font-bold '>Chicken </p>
+    <p className='text-lg font-medium '>20$</p>
+       
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+        </Container>
+     </div>
   )
 }
 
